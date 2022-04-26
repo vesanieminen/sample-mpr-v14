ebd487e2863320be1ba6f1077cfb07cc22dd712d
Vesa Nieminen <vesa@vaadin.com>
2022-04-26 17:21:16 +0300

    Added Push and MprTheme

---
+++ src/main/java/org/test/views/AppShell.java
@@ -0,0 +1,12 @@
+package org.test.views;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.shared.ui.Transport;
+import com.vaadin.mpr.core.MprTheme;
+
+@MprTheme("mytheme")
+@Push()
+public class AppShell implements AppShellConfigurator {
+
+}
